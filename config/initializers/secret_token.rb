d3d21cafa50ef4f574574d5fda9ae76348a639c8
Josh Steiner <josh@jsteiner.me>
2012-06-07 09:47:34 -0400

    rails new descepto -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Discepto::Application.config.secret_token = '1e0bba084afe844e1a4ee4de91fc6a329c9cf91eb66d02202fdacd79d70d1266dffeea95b224fa9dba7e83e5567daf9f966d2444aebbdbf1ebe7ab65a0145196'
