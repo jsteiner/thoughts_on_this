e682dc2728fc7a388422c3db32d4ad3b95ab55e2
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-06-15 14:13:48 -0400

    Add missing tests for ImageSubject.

--- spec/models/image_subject_spec.rb
+++ spec/models/image_subject_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe ImageSubject do
+  it { should have_one(:discussion) }
+  it { should allow_mass_assignment_of(:image) }
+  it { should accept_nested_attributes_for(:discussion) }
+
   it { should have_attached_file(:image) }
   it { should validate_attachment_presence(:image) }
   it { should validate_attachment_content_type(:image).
