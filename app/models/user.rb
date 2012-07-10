cba95caa0f7e37fd8c9dc14120152696867d1000
Josh Steiner <josh@jsteiner.me>
2012-07-10 15:25:05 -0400

    Add discussion_timeline method to be used by Edward

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_many :discussions, dependent: :destroy
   attr_accessible :first_name, :last_name
 
+  def discussion_timeline
+    discussions.order('updated_at DESC')
+  end
+
   def full_name
     if has_full_name?
       "#{first_name} #{last_name}"
