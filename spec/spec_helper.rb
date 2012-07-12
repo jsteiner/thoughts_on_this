cef1ea82be09fb9106880d3e42ed4146f91f9e43
Josh Steiner <josh@jsteiner.me>
2012-07-11 21:58:14 -0400

    Move javascript out of html file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,7 @@ Spork.prefork do
   require 'paperclip/matchers'
   RSpec.configure do |config|
     config.mock_with :mocha
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
+    config.use_transactional_fixtures = false
     config.infer_base_class_for_anonymous_controllers = false
     config.include FactoryGirl::Syntax::Methods
     config.include Paperclip::Shoulda::Matchers
