b1c2956e025d7d18b606bc49b7dab78b98382f36
Edward Loveall <edward@edwardloveall.com>
2012-07-13 15:13:55 -0400

    Colors

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('li.dropdown').click(function() {
     $(this).children('.menu-content').show();
   })
+
+  $('.main-wrap').click(function() {
+    $('.menu-content').hide();
+  })
 });
