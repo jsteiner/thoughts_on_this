34ae192ea78274a6d024307c3176576cf00eb69a
Edward Loveall <edward@edwardloveall.com>
2012-07-18 14:20:17 -0400

    Image now stays at the center of the viewport

--- app/assets/javascripts/discussions/discussion.js
+++ app/assets/javascripts/discussions/discussion.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+  v_align_image();
+
   $('a.share').toggle(
     function(e) {
-      $(this).parent().next('li').show();
+      $(this).parent().prev('li').show();
       e.preventDefault();
     },
     function(e) {
-      $(this).parent().next('li').hide();
+      $(this).parent().prev('li').hide();
       e.preventDefault();
     }
   );
+
+  $(window).resize(function () {
+    v_align_image();
+  });
+
+  function v_align_image() {
+    var wrapper_height = $('.visual').height() * 0.5,
+        image_height = $('.visual > img').height() * 0.5,
+        target_margin_top = wrapper_height - image_height;
+    target_margin_top = Math.max(target_margin_top, 0) + "px";
+
+    $('.visual > img').css('margin-top', target_margin_top);
+  }
 });
