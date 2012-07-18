4454003cec51926b915184b64eb02c7268f5f902
Edward Loveall <edward@edwardloveall.com>
2012-07-17 23:29:17 -0400

    WIP images look better, working on raw view

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,5 +1,10 @@
 module DiscussionsHelper
-  def truncate_title(title)
-    truncate(title, length: 70, omission: '...')
+  def raw_subject_url(discussion)
+    case discussion.subject_type
+      when "ImageSubject"
+        discussion.subject.image
+      when "TextSubject"
+        discussion.subject.content
+    end
   end
 end
