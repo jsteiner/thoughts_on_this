ec2566bdba120184b3b01a95924e3ef98d8879b3
Josh Steiner <josh@jsteiner.me>
2012-08-02 19:28:37 -0400

    Configure s3

--- app/models/image_subject.rb
+++ app/models/image_subject.rb
@@ -3,10 +3,9 @@ class ImageSubject < ActiveRecord::Base
 
   belongs_to :user
   has_one :discussion, as: :subject
-  has_attached_file :image
+  has_attached_file :image, PaperclipStorageOptions
 
   accepts_nested_attributes_for :discussion
-
 
   validates_attachment :image, presence: true,
                        content_type: { content_type: ['image/jpeg', 'image/png', 'image/gif'] },
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,6 @@ ThoughtsOnThis::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  PaperclipStorageOptions = {}
 end
